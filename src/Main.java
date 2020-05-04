6be29590ebf0c2eeec4ec152a1c9ceb3c64363ce
illmapamelairani <ilmapamela0123@gmail.com>
2020-05-04 08:11:48 +0800

    tugas pertemuan 05

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        handphone objek = new handphone();
 
-        //player
-        class Player{
-          String nama;
-          int level;
-          int score;
+        objek.merek = "iPhone 11 Pro Max";
+        objek.spesifikasi = "tinggi : 158,0 mm, lebar : 77,8 mm, tebal : 8,1 mm, berat : 336 gram";
+        objek.warna = "silver";
+        objek.harga = 22999000;
+        objek.tahun = 2019;
 
-        }
-        System.out.println("t");
+        objek.video();
+        objek.foto();
+        objek.internetan();
     }
 }
--- src/handphone.java
+++ src/handphone.java
@@ -1,8 +1,35 @@
 public class handphone {
 
+    //atribute
     String merek;
     String spesifikasi;
     String warna;
     int harga;
+    int tahun;
+
+    public handphone(){ //object
+
+    }
+
+    //behavior
+    void video(){
+        System.out.println("kamu bisa mengabadikan momen indah bersama orang tersayang dengan membuat video");
+        System.out.println("merek handphone adalah : "+merek);
+        System.out.println("spesifikasi handphone : "+spesifikasi);
+        System.out.println("warna handphone : "+warna);
+        System.out.println("harga handphone :"+harga);
+        System.out.println("tahun peluncuran handphone ke publik : "+tahun);
+        System.out.println();
+    }
+    void foto(){
+        System.out.println("bukan hanya video kamu juga bisa mengabadikan momen indah bersama orang tersayang kamu :)");
+        System.out.println("ingat waktu tidak bisa diulang jadi sebelum kamu kehilangan momen tersebut");
+        System.out.println();
+    }
+    void internetan(){
+        System.out.println("dengan internet kamu bisa melakukan banyak hal");
+        System.out.println("seperti bermain games, bermain sosial media, mencari informasi,");
+        System.out.println("dan banyak hal yang bisa kalian lakukan");
+    }
 
 }
